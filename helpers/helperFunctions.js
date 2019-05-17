d7b592f27ce2818a125ecc5dc367727636697079
geoboom <leegeorgie52@gmail.com>
2019-05-17 11:26:55 +0800

    fixed trip date timezone issue

--- helpers/helperFunctions.js
+++ helpers/helperFunctions.js
@@ -6,8 +6,7 @@ const startTime = '00:00:00';
 const endTime = '10:00:00';
 
 const getCurrTripDate = () => (
-  moment(moment(), timeFormat)
-    .utcOffset('+0800')
+  moment(moment().utcOffset('+0800'), timeFormat)
     .isBetween(moment(startTime, timeFormat), moment(endTime, timeFormat))
     ? moment() : moment().add(1, 'd')
 );
